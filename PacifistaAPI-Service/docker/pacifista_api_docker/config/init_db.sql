cea99ea57a76278c4d2b6e23d03663b24fe71e45
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-07-28 17:44:53 +0200

    Updated docker env, added funix api

--- PacifistaAPI-Service/docker/pacifista_api_docker/config/init_db.sql
+++ PacifistaAPI-Service/docker/pacifista_api_docker/config/init_db.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS pacifista_api;
 CREATE DATABASE IF NOT EXISTS pacifista_api_docker;
+CREATE DATABASE IF NOT EXISTS funixgaming_web_docker;
 
-CREATE USER 'pacifista'@'%' IDENTIFIED BY 'pacifista';
-GRANT ALL PRIVILEGES ON *.* TO 'pacifista'@'%';
+CREATE USER 'funix'@'%' IDENTIFIED BY 'funix';
+GRANT ALL PRIVILEGES ON *.* TO 'funix'@'%';
