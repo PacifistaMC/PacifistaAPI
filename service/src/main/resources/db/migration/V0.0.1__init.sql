b5977256256807e2931037427a05312d3bb97166
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-04-11 17:29:47 +0200

    migration

--- service/src/main/resources/db/migration/V0.0.1__init.sql
+++ service/src/main/resources/db/migration/V0.0.1__init.sql
@@ -249,3 +249,18 @@ create table shop_articles
     price            double precision not null,
     category_id      bigint           not null constraint category_id_link_to_article references shop_categories
 );
+
+create table pacifista_news
+(
+    id                bigserial primary key,
+    created_at        timestamp(6)   not null,
+    updated_at        timestamp(6),
+    uuid              varchar(255)   not null constraint uk_public_id_pacifista_news unique,
+    article_image_url varchar(2000)  not null,
+    body              varchar(20000) not null,
+    name              varchar(255)   not null,
+    original_writer   varchar(255)   not null,
+    subtitle          varchar(255)   not null,
+    title             varchar(255)   not null,
+    update_writer     varchar(255)
+);
