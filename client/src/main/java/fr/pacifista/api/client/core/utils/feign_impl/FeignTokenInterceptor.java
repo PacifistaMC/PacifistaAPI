54ecedac3700f2310ef260fcbff88b356a9b32bd
FunixG <antoine.pronnier@gmail.com>
2023-05-17 15:24:56 +0200

    add funixprod token in request

--- client/src/main/java/fr/pacifista/api/client/core/utils/feign_impl/FeignTokenInterceptor.java
+++ client/src/main/java/fr/pacifista/api/client/core/utils/feign_impl/FeignTokenInterceptor.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpHeaders;
 
 public class FeignTokenInterceptor implements RequestInterceptor {
 
+    private final String barerToken = System.getenv("FUNIXPRODUCTIONS_API_TOKEN");
+
     @Override
     public void apply(RequestTemplate requestTemplate) {
-        requestTemplate.header(HttpHeaders.AUTHORIZATION, "Bearer token");
+        requestTemplate.header(HttpHeaders.AUTHORIZATION, "Bearer " + barerToken);
     }
 }
