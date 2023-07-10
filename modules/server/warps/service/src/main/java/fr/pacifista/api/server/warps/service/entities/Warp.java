17850d50ec561fa1f0dd51c1530b79473b7e2f61
FunixG <antoine.pronnier@gmail.com>
2023-07-10 15:27:41 +0200

    Done warps module

---
+++ modules/server/warps/service/src/main/java/fr/pacifista/api/server/warps/service/entities/Warp.java
@@ -0,0 +1,49 @@
+package fr.pacifista.api.server.warps.service.entities;
+
+import fr.pacifista.api.core.service.entities.Location;
+import fr.pacifista.api.server.warps.client.enums.WarpType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity(name = "pacifista_warps")
+public class Warp extends Location {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, name = "warp_item")
+    private String warpItem;
+
+    @Column(nullable = false)
+    private Boolean publicAccess = true;
+
+    @Column(nullable = false, name = "player_owner_uuid")
+    private String playerOwnerUuid;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WarpType type;
+
+    public void setPlayerOwnerUuid(UUID playerOwnerUuid) {
+        if (playerOwnerUuid != null) {
+            this.playerOwnerUuid = playerOwnerUuid.toString();
+        } else {
+            this.playerOwnerUuid = null;
+        }
+    }
+
+    public UUID getPlayerOwnerUuid() {
+        if (this.playerOwnerUuid != null) {
+            return UUID.fromString(this.playerOwnerUuid);
+        } else {
+            return null;
+        }
+    }
+}
