0d53e3d94d26f3bdac61d8102d3846c3e78b7f12
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-02-07 10:13:28 +0100

    add forgotten migration

---
+++ modules/server/box/service/src/main/resources/db/migration/V15__add_files_storages_to_web_shop.sql
@@ -0,0 +1,4 @@
+ALTER TABLE shop_articles ADD file_name VARCHAR(255) NOT NULL;
+ALTER TABLE shop_articles ADD file_path VARCHAR(255) NOT NULL;
+ALTER TABLE shop_articles ADD file_extension VARCHAR(255) NOT NULL;
+ALTER TABLE shop_articles ADD file_size bigint NOT NULL;
