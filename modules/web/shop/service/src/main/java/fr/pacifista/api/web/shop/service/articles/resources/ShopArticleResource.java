fb80163daba74d815708af1def5979baff203c27
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-02-13 13:56:22 +0100

    article ressource clean

--- modules/web/shop/service/src/main/java/fr/pacifista/api/web/shop/service/articles/resources/ShopArticleResource.java
+++ modules/web/shop/service/src/main/java/fr/pacifista/api/web/shop/service/articles/resources/ShopArticleResource.java
@@ -8,8 +8,11 @@ import com.funixproductions.core.tools.pdf.tools.VATInformation;
 import fr.pacifista.api.web.shop.client.articles.clients.ShopArticlesClient;
 import fr.pacifista.api.web.shop.client.articles.dtos.ShopArticleDTO;
 import fr.pacifista.api.web.shop.service.articles.services.ShopArticleService;
+import lombok.NonNull;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/web/shop/articles")
@@ -26,25 +29,20 @@ public class ShopArticleResource extends ApiStorageResource<ShopArticleDTO, Shop
     @Override
     public PageDTO<ShopArticleDTO> getAll(String page, String elemsPerPage, String search, String sort) {
         final PageDTO<ShopArticleDTO> result = super.getAll(page, elemsPerPage, search, sort);
-        final VATInformation vatInformation = getVat();
 
-        for (ShopArticleDTO article : result.getContent()) {
-            article.setTax(article.getPrice() * (vatInformation.getVatRate() / 100));
-            article.setPriceWithTax(article.getPrice() + article.getTax());
-        }
+        setTaxInDto(result.getContent());
         return result;
     }
 
     @Override
     public ShopArticleDTO findById(String id) {
         final ShopArticleDTO articleDTO = super.findById(id);
-        final VATInformation vatInformation = getVat();
 
-        articleDTO.setTax(articleDTO.getPrice() * (vatInformation.getVatRate() / 100));
-        articleDTO.setPriceWithTax(articleDTO.getPrice() + articleDTO.getTax());
+        setTaxInDto(List.of(articleDTO));
         return articleDTO;
     }
 
+    @NonNull
     private VATInformation getVat() {
         final UserDTO user = currentSession.getCurrentUser();
         VATInformation vatInformation = VATInformation.FRANCE;
@@ -52,9 +50,16 @@ public class ShopArticleResource extends ApiStorageResource<ShopArticleDTO, Shop
         if (user != null) {
             vatInformation = VATInformation.getVATInformation(user.getCountry().getCountryCode2Chars());
         }
-        if (vatInformation == null) {
-            vatInformation = VATInformation.FRANCE;
+
+        return vatInformation == null ? VATInformation.FRANCE : vatInformation;
+    }
+
+    private void setTaxInDto(final Iterable<ShopArticleDTO> articles) {
+        final VATInformation vatInformation = getVat();
+
+        for (ShopArticleDTO articleDTO : articles) {
+            articleDTO.setTax(articleDTO.getPrice() * (vatInformation.getVatRate() / 100));
+            articleDTO.setPriceWithTax(articleDTO.getPrice() + articleDTO.getTax());
         }
-        return vatInformation;
     }
 }
