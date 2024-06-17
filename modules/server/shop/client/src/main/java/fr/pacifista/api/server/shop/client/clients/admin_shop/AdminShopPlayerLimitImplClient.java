c770290d585f3587df31dd763be471dd0da9b303
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-06-17 13:30:38 +0200

    add feign impl method reset all

--- modules/server/shop/client/src/main/java/fr/pacifista/api/server/shop/client/clients/admin_shop/AdminShopPlayerLimitImplClient.java
+++ modules/server/shop/client/src/main/java/fr/pacifista/api/server/shop/client/clients/admin_shop/AdminShopPlayerLimitImplClient.java
@@ -1,9 +1,10 @@
 package fr.pacifista.api.server.shop.client.clients.admin_shop;
 
+import feign.FeignException;
 import fr.pacifista.api.core.client.clients.FeignImpl;
 import fr.pacifista.api.server.shop.client.dtos.admin_shop.AdminShopPlayerLimitDTO;
 
-public class AdminShopPlayerLimitImplClient extends FeignImpl<AdminShopPlayerLimitDTO, AdminShopPlayerLimitClient> {
+public class AdminShopPlayerLimitImplClient extends FeignImpl<AdminShopPlayerLimitDTO, AdminShopPlayerLimitClient> implements AdminShopPlayerLimitClient {
 
     public static final String PATH = "shop/admin/playerlimit";
 
@@ -11,4 +12,12 @@ public class AdminShopPlayerLimitImplClient extends FeignImpl<AdminShopPlayerLim
         super(PATH, AdminShopPlayerLimitClient.class);
     }
 
+    @Override
+    public void resetPlayerLimits() {
+        try {
+            super.getClient().resetPlayerLimits();
+        } catch (FeignException e) {
+            throw handleFeignException(e);
+        }
+    }
 }
