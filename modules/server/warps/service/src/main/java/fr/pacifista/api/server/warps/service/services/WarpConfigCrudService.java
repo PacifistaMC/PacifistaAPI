6cf2de0cd69d2fafa9e3f5ffb4aad7979aafc7e9
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-10-09 18:22:58 +0200

    wip

--- modules/server/warps/service/src/main/java/fr/pacifista/api/server/warps/service/services/WarpConfigCrudService.java
+++ modules/server/warps/service/src/main/java/fr/pacifista/api/server/warps/service/services/WarpConfigCrudService.java
@@ -1,28 +1,48 @@
 package fr.pacifista.api.server.warps.service.services;
 
 import com.funixproductions.core.crud.services.ApiService;
+import com.funixproductions.core.exceptions.ApiBadRequestException;
+import com.funixproductions.core.exceptions.ApiNotFoundException;
 import fr.pacifista.api.server.warps.client.dtos.WarpConfigDTO;
+import fr.pacifista.api.server.warps.service.entities.Warp;
 import fr.pacifista.api.server.warps.service.entities.WarpConfig;
 import fr.pacifista.api.server.warps.service.mappers.WarpConfigMapper;
 import fr.pacifista.api.server.warps.service.repositories.WarpConfigRepository;
+import fr.pacifista.api.server.warps.service.repositories.WarpRepository;
 import lombok.NonNull;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class WarpConfigCrudService extends ApiService<WarpConfigDTO, WarpConfig, WarpConfigMapper, WarpConfigRepository> {
 
-    private final WarpCrudService warpCrudService;
+    private final WarpRepository warpRepository;
 
     public WarpConfigCrudService(WarpConfigRepository repository,
                                  WarpConfigMapper mapper,
-                                 WarpCrudService warpCrudService) {
+                                 WarpRepository warpRepository) {
         super(repository, mapper);
-        this.warpCrudService = warpCrudService;
+        this.warpRepository = warpRepository;
     }
 
     @Override
     public void afterMapperCall(@NonNull WarpConfigDTO dto, @NonNull WarpConfig entity) {
-        entity.setWarp(warpCrudService.getWarp(dto.getWarp().getId()));
+        entity.setWarp(this.getWarp(dto.getWarp().getId()));
+    }
+
+    @NotNull
+    protected Warp getWarp(UUID warpId) {
+        if (warpId == null) {
+            throw new ApiBadRequestException("L'id du warp est requis");
+        }
+
+        final Warp warp = this.warpRepository.findByUuid(warpId.toString()).orElse(null);
+        if (warp == null) {
+            throw new ApiNotFoundException("Le warp avec l'id " + warpId + " n'existe pas");
+        }
+        return warp;
     }
 
 }
--- modules/server/warps/service/src/main/java/fr/pacifista/api/server/warps/service/services/WarpCrudService.java
+++ modules/server/warps/service/src/main/java/fr/pacifista/api/server/warps/service/services/WarpCrudService.java
@@ -3,11 +3,10 @@ package fr.pacifista.api.server.warps.service.services;
 import com.funixproductions.core.crud.services.ApiService;
 import com.funixproductions.core.exceptions.ApiBadRequestException;
 import com.funixproductions.core.exceptions.ApiNotFoundException;
+import fr.pacifista.api.server.warps.client.dtos.WarpConfigDTO;
 import fr.pacifista.api.server.warps.client.dtos.WarpDTO;
 import fr.pacifista.api.server.warps.service.entities.Warp;
-import fr.pacifista.api.server.warps.service.mappers.WarpConfigMapper;
 import fr.pacifista.api.server.warps.service.mappers.WarpMapper;
-import fr.pacifista.api.server.warps.service.repositories.WarpConfigRepository;
 import fr.pacifista.api.server.warps.service.repositories.WarpPlayerInteractionRepository;
 import fr.pacifista.api.server.warps.service.repositories.WarpPortalRepository;
 import fr.pacifista.api.server.warps.service.repositories.WarpRepository;
@@ -23,47 +22,51 @@ import java.util.UUID;
 @Service
 public class WarpCrudService extends ApiService<WarpDTO, Warp, WarpMapper, WarpRepository> {
 
-    private final WarpConfigMapper warpConfigMapper;
-
-    private final WarpConfigRepository warpConfigRepository;
+    private final WarpConfigCrudService warpConfigCrudService;
     private final WarpPlayerInteractionRepository warpPlayerInteractionRepository;
     private final WarpPortalRepository warpPortalRepository;
 
     public WarpCrudService(WarpRepository repository,
                            WarpMapper mapper,
-                           WarpConfigRepository warpConfigRepository,
-                           WarpConfigMapper warpConfigMapper,
+                           WarpConfigCrudService warpConfigCrudService,
                            WarpPlayerInteractionRepository warpPlayerInteractionRepository,
                            WarpPortalRepository warpPortalRepository) {
         super(repository, mapper);
-        this.warpConfigRepository = warpConfigRepository;
-        this.warpConfigMapper = warpConfigMapper;
+        this.warpConfigCrudService = warpConfigCrudService;
         this.warpPlayerInteractionRepository = warpPlayerInteractionRepository;
         this.warpPortalRepository = warpPortalRepository;
     }
 
-    @Override
     @Transactional
+    @Override
     public @NonNull WarpDTO create(WarpDTO request) {
         final WarpDTO warpDTO = super.create(request);
 
-        if(warpDTO.getConfig() == null) {
-            warpDTO.setConfig(this.warpC);
+        if (warpDTO.getConfig() == null) {
+            warpDTO.setConfig(this.warpConfigCrudService.create(WarpConfigDTO.initWithDefaults(warpDTO)));
         }
         return warpDTO;
     }
 
-    @Override
     @Transactional
+    @Override
     public List<WarpDTO> create(List<@Valid WarpDTO> request) {
         final List<WarpDTO> res = super.create(request);
+
+        for (WarpDTO warpDTO : res) {
+            if (warpDTO.getConfig() == null) {
+                warpDTO.setConfig(this.warpConfigCrudService.create(WarpConfigDTO.initWithDefaults(warpDTO)));
+            }
+        }
+
+        return res;
     }
 
     @Override
     public void beforeDeletingEntity(@NonNull Iterable<Warp> entity) {
         this.warpPortalRepository.deleteAllByWarpIn(entity);
         this.warpPlayerInteractionRepository.deleteAllByWarpIn(entity);
-        this.warpConfigRepository.deleteAllByWarpIn(entity);
+        this.warpConfigCrudService.getRepository().deleteAllByWarpIn(entity);
     }
 
     @NotNull
