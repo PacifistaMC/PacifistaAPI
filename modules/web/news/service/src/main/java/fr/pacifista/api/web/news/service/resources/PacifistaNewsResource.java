2f84bef683e492a915e2288e9d2a107be12dfc9b
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-11-12 14:00:04 +0100

    WIP Services

--- modules/web/news/service/src/main/java/fr/pacifista/api/web/news/service/resources/PacifistaNewsResource.java
+++ modules/web/news/service/src/main/java/fr/pacifista/api/web/news/service/resources/PacifistaNewsResource.java
@@ -1,26 +1,37 @@
 package fr.pacifista.api.web.news.service.resources;
 
+import com.funixproductions.api.user.client.dtos.UserDTO;
 import com.funixproductions.api.user.client.dtos.UserSession;
 import com.funixproductions.api.user.client.enums.UserRole;
 import com.funixproductions.api.user.client.security.CurrentSession;
 import com.funixproductions.core.crud.dtos.PageDTO;
+import com.funixproductions.core.crud.enums.SearchOperation;
+import com.funixproductions.core.exceptions.ApiBadRequestException;
 import com.funixproductions.core.exceptions.ApiNotFoundException;
+import com.funixproductions.core.exceptions.ApiUnauthorizedException;
+import com.funixproductions.core.files.services.ApiStorageService;
 import com.funixproductions.core.tools.network.IPUtils;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import fr.pacifista.api.web.news.client.clients.PacifistaNewsClient;
 import fr.pacifista.api.web.news.client.dtos.news.PacifistaNewsDTO;
+import fr.pacifista.api.web.news.client.dtos.news.PacifistaNewsImageDTO;
 import fr.pacifista.api.web.news.client.dtos.news.PacifistaNewsLikeDTO;
 import fr.pacifista.api.web.news.service.entities.news.PacifistaNews;
 import fr.pacifista.api.web.news.service.services.news.PacifistaNewsCrudService;
 import fr.pacifista.api.web.news.service.services.news.PacifistaNewsImageCrudService;
 import fr.pacifista.api.web.news.service.services.news.PacifistaNewsLikeCrudService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -55,6 +66,18 @@ public class PacifistaNewsResource implements PacifistaNewsClient {
 
     @Override
     public PageDTO<PacifistaNewsDTO> getAll(int page) {
+        final String search = this.canActualUserSeeDrafts() ? "" : String.format(
+                "draft:%s:%s",
+                SearchOperation.IS_FALSE.getOperation(),
+                "false"
+        );
+
+        return this.newsService.getAll(
+                Integer.toString(page),
+                "20",
+                search,
+                "createdAt:desc"
+        );
     }
 
     @Override
@@ -86,14 +109,58 @@ public class PacifistaNewsResource implements PacifistaNewsClient {
 
     @Override
     public PacifistaNewsDTO createNews(PacifistaNewsDTO newsDTO, MultipartFile image) {
+        final PacifistaNewsDTO createdNews = this.newsService.create(newsDTO);
+
+        try {
+            final PacifistaNewsImageDTO imageDTO = this.imageService.store(new PacifistaNewsImageDTO(createdNews, false), image);
+            final PacifistaNewsImageDTO imageLowResDTO = this.imageService.store(
+                    new PacifistaNewsImageDTO(createdNews, true),
+                    this.createLowerResolutionFile(image)
+            );
+
+            createdNews.setArticleImageId(imageDTO.getId());
+            createdNews.setArticleImageIdLowRes(imageLowResDTO.getId());
+            return this.newsService.updatePut(createdNews);
+        } catch (Exception e) {
+            this.newsService.delete(createdNews.getId().toString());
+            throw e;
+        }
     }
 
     @Override
     public PacifistaNewsDTO updateNews(PacifistaNewsDTO newsDTO) {
+        return this.newsService.updatePut(newsDTO);
     }
 
     @Override
     public PacifistaNewsDTO updateNews(PacifistaNewsDTO newsDTO, MultipartFile image) {
+        final PacifistaNewsDTO oldNews = this.newsService.findById(newsDTO.getId().toString());
+        final PacifistaNewsDTO updatedNews = this.newsService.updatePut(newsDTO);
+
+        try {
+            final PacifistaNewsImageDTO imageDTO = this.imageService.store(new PacifistaNewsImageDTO(updatedNews, false), image);
+            final PacifistaNewsImageDTO imageLowResDTO = this.imageService.store(
+                    new PacifistaNewsImageDTO(updatedNews, true),
+                    this.createLowerResolutionFile(image)
+            );
+
+            updatedNews.setArticleImageId(imageDTO.getId());
+            updatedNews.setArticleImageIdLowRes(imageLowResDTO.getId());
+
+            try {
+                final PacifistaNewsDTO newOne = this.newsService.updatePut(updatedNews);
+                this.imageService.delete(oldNews.getArticleImageId().toString());
+                this.imageService.delete(oldNews.getArticleImageIdLowRes().toString());
+                return newOne;
+            } catch (Exception e) {
+                this.imageService.delete(imageDTO.getId().toString());
+                this.imageService.delete(imageLowResDTO.getId().toString());
+                throw e;
+            }
+        } catch (Exception e) {
+            this.newsService.updatePut(oldNews);
+            throw e;
+        }
     }
 
     @Override
@@ -103,14 +170,59 @@ public class PacifistaNewsResource implements PacifistaNewsClient {
 
     @Override
     public PageDTO<PacifistaNewsLikeDTO> getLikesOnNews(String newsId, int page) {
+        final PacifistaNewsDTO news = this.newsService.findById(newsId);
+
+        if (Boolean.TRUE.equals(news.getDraft()) && !this.canActualUserSeeDrafts()) {
+            throw new ApiNotFoundException(String.format("L'entité id %s n'existe pas.", newsId));
+        }
+
+        return this.likeService.getAll(
+                Integer.toString(page),
+                "20",
+                String.format(
+                        "news.uuid:%s:%s",
+                        SearchOperation.EQUALS.getOperation(),
+                        newsId
+                ),
+                "createdAt:desc"
+        );
     }
 
     @Override
     public PacifistaNewsLikeDTO likeNews(String newsId) {
+        final UserDTO currentUser = this.actualSession.getCurrentUser();
+        if (currentUser == null) {
+            throw new ApiUnauthorizedException("Vous devez être connecté pour aimer une news.");
+        }
+
+        final PacifistaNewsLikeDTO likedNews = this.getLikeForUserAndNews(currentUser, newsId);
+
+        if (likedNews != null) {
+            throw new ApiBadRequestException("Vous avez déjà aimé cette news.");
+        } else {
+            final PacifistaNewsDTO news = new PacifistaNewsDTO();
+            news.setId(UUID.fromString(newsId));
+            final PacifistaNewsLikeDTO like = new PacifistaNewsLikeDTO();
+            like.setNews(news);
+
+            return this.likeService.create(like);
+        }
     }
 
     @Override
     public void removeLikeOnNews(String newsId) {
+        final UserDTO currentUser = this.actualSession.getCurrentUser();
+        if (currentUser == null) {
+            throw new ApiUnauthorizedException("Vous devez être connecté pour aimer une news.");
+        }
+
+        final PacifistaNewsLikeDTO likedNews = this.getLikeForUserAndNews(currentUser, newsId);
+
+        if (likedNews != null) {
+            this.likeService.delete(likedNews.getId().toString());
+        } else {
+            throw new ApiBadRequestException("Vous n'avez pas encore aimé cette news.");
+        }
     }
 
     private boolean canActualUserSeeDrafts() {
@@ -124,4 +236,76 @@ public class PacifistaNewsResource implements PacifistaNewsClient {
                     currentSession.getUserDTO().getRole() == UserRole.PACIFISTA_MODERATOR;
         }
     }
+
+    @Nullable
+    private PacifistaNewsLikeDTO getLikeForUserAndNews(final UserDTO user, final String newsId) {
+        try {
+            return this.likeService.getAll(
+                    "0",
+                    "1",
+                    String.format(
+                            "news.uuid:%s:%s,funixProdUserId:%s:%s",
+                            SearchOperation.EQUALS.getOperation(),
+                            newsId,
+                            SearchOperation.EQUALS.getOperation(),
+                            user.getId()
+                    ),
+                    "createdAt:desc"
+            ).getContent().getFirst();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
+    private MultipartFile createLowerResolutionFile(MultipartFile original) {
+        final byte[] bytes = ApiStorageService.createThumbnailFromImage(original, 200);
+
+        return new MultipartFile() {
+            @NotNull
+            @Override
+            public String getName() {
+                return original.getName();
+            }
+
+            @Override
+            public String getOriginalFilename() {
+                return original.getOriginalFilename();
+            }
+
+            @Override
+            public String getContentType() {
+                return original.getContentType();
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return bytes.length == 0;
+            }
+
+            @Override
+            public long getSize() {
+                return bytes.length;
+            }
+
+            @NotNull
+            @Override
+            public byte[] getBytes() throws IOException {
+                return bytes;
+            }
+
+            @NotNull
+            @Override
+            public InputStream getInputStream() throws IOException {
+                return new ByteArrayInputStream(bytes);
+            }
+
+            @Override
+            public void transferTo(@NotNull File dest) throws IOException {
+                try (FileOutputStream fos = new FileOutputStream(dest)) {
+                    fos.write(bytes);
+                }
+            }
+        };
+    }
+
 }
