c8644369e7086fd9aa18d8f7554193f34aea2e85
FunixG <antoine.pronnier@gmail.com>
2024-12-07 14:42:06 +0100

    NEWS - Fix get news by name

--- modules/web/news/service/src/main/java/fr/pacifista/api/web/news/service/repositories/news/PacifistaNewsRepository.java
+++ modules/web/news/service/src/main/java/fr/pacifista/api/web/news/service/repositories/news/PacifistaNewsRepository.java
@@ -4,6 +4,11 @@ import com.funixproductions.core.crud.repositories.ApiRepository;
 import fr.pacifista.api.web.news.service.entities.news.PacifistaNews;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PacifistaNewsRepository extends ApiRepository<PacifistaNews> {
+
+    Optional<PacifistaNews> findByNameIgnoreCase(String name);
+
 }
