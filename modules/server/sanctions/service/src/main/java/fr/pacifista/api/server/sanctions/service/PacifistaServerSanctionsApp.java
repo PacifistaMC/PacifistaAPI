d4860233af2f9657fd807a5eb83a5fed43df8011
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-12-07 22:30:09 +0100

    1.5.6.1 - Fix encryption, MEP error fix

--- modules/server/sanctions/service/src/main/java/fr/pacifista/api/server/sanctions/service/PacifistaServerSanctionsApp.java
+++ modules/server/sanctions/service/src/main/java/fr/pacifista/api/server/sanctions/service/PacifistaServerSanctionsApp.java
@@ -1,12 +1,13 @@
 package fr.pacifista.api.server.sanctions.service;
 
+import com.funixproductions.api.encryption.client.clients.EncryptionClient;
 import com.funixproductions.api.user.client.clients.UserAuthClient;
 import fr.pacifista.api.core.service.tools.discord.clients.DiscordWebhookClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(clients = {UserAuthClient.class, DiscordWebhookClient.class})
+@EnableFeignClients(clients = {UserAuthClient.class, DiscordWebhookClient.class, EncryptionClient.class})
 @SpringBootApplication(scanBasePackages = {
         "com.funixproductions",
         "fr.pacifista"
