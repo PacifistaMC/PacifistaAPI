e0f42159fce933a590bce17270405a0e8598e10c
Antoine PRONNIER <antoine.pronnier@gmail.com>
2025-02-03 09:24:24 +0100

    Fix migration

--- modules/server/box/service/src/main/resources/db/migration/V56__jobs_change_amount_collected_items_to_int.sql
+++ modules/server/box/service/src/main/resources/db/migration/V56__jobs_change_amount_collected_items_to_int.sql
@@ -1,8 +1,10 @@
 ALTER TABLE job_player_task
+    ALTER COLUMN collected_task_items DROP DEFAULT;
+
+ALTER TABLE job_player_task
     ALTER COLUMN collected_task_items
         SET DATA TYPE INT
         USING (CASE WHEN collected_task_items THEN 99999 ELSE 0 END);
 
 ALTER TABLE job_player_task
-    ALTER COLUMN collected_task_items
-        SET DEFAULT 0;
+    ALTER COLUMN collected_task_items SET DEFAULT 0;
