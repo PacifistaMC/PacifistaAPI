79f8442b93d9c57a9cbc160b55d90f0b6cd6dffe
Antoine PRONNIER <antoine.pronnier@gmail.com>
2025-02-11 11:58:32 +0100

    Add latency and afk status on dto for status

--- modules/server/essentials/client/src/main/java/fr/pacifista/api/server/essentials/client/status/dtos/PacifistaServerInfoDTO.java
+++ modules/server/essentials/client/src/main/java/fr/pacifista/api/server/essentials/client/status/dtos/PacifistaServerInfoDTO.java
@@ -36,6 +36,33 @@ public class PacifistaServerInfoDTO {
         private Integer playerSlots;
 
         private List<HttpPlayerConnected> players;
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof final Server server) {
+                return this.name.equals(server.getName()) && this.proxyName.equals(server.getProxyName());
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode() + this.name.hashCode() + this.proxyName.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return String.format(
+                    "Server{name=%s, proxyName=%s, isOnline=%b, onlinePlayers=%d, playerSlots=%d, players=%s}",
+                    this.name,
+                    this.proxyName,
+                    this.isOnline,
+                    this.onlinePlayers,
+                    this.playerSlots,
+                    this.players
+            );
+        }
     }
 
     @Getter
@@ -43,9 +70,39 @@ public class PacifistaServerInfoDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class HttpPlayerConnected {
+
         private String name;
 
         private UUID uuid;
+
+        private Integer latency;
+
+        private Boolean afk;
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof final HttpPlayerConnected httpPlayerConnected) {
+                return this.uuid.equals(httpPlayerConnected.getUuid());
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode() + this.uuid.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return String.format(
+                    "HttpPlayerConnected{name=%s, uuid=%s, latency=%d, afk=%b}",
+                    this.name,
+                    this.uuid,
+                    this.latency,
+                    this.afk
+            );
+        }
     }
 
     @Override
